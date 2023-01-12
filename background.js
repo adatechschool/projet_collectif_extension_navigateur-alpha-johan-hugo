e20f34dc8c7c89d703e493b0633e54b4ebb62a8c
hugorince <hugo.rince@gmail.com>
2023-01-12 15:07:10 +0100

    recuperation des secondes dans le pop-up

--- background.js
+++ background.js
@@ -1,48 +1,37 @@
 let seconds = 0;
+let interval = null;
 
 function counter() {
     seconds++;
   }
 
-let interval = null;
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  if (tab.url.startsWith('https://www.youtube.com/')) {
+      console.log('Youtube detected');
+      interval = setInterval(counter, 1000)
+  }
+  else {
+    clearInterval(interval);
+    console.log('new tab selected');
+    if (seconds != 0){
+        
+  }
+  console.log(seconds)
+}
+}
 
-
+chrome.tabs.onActivated.addListener(getCurrentTab);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.message === "messageSent")
-    {
-      console.log('bien reçu')
+    if (request.greeting === "hello")
+      console.log('bien reçu');
+      sendResponse({timeSpent: seconds}); 
+      return true;
     }
-    sendResponse({farewell: "goodbye"});
-    return true 
-  }
 );
-
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    if (tab.url.startsWith('https://www.youtube.com/')) {
-        console.log('Youtube detected');
-        interval = setInterval(counter, 1000)
-    }
-    else {
-      clearInterval(interval);
-      console.log('new tab selected');
-      if (seconds != 0){
-         
-    }
-    console.log(seconds)
-}
-}
-
-
-//chrome.tabs.onCreated.addListener(create);
-chrome.tabs.onActivated.addListener(getCurrentTab);
-//chrome.tabs.onUpdated.addListener(getCurrentTab);
-
-
 
 
 
--- popup.js
+++ popup.js
@@ -1,30 +1,6 @@
-document.getElementById('compteur').innerHTML = 'yoyoyo'
+(async () => {
+    const response = await chrome.runtime.sendMessage({greeting: "hello"});
+    // do something with response here, not outside the function
+    document.getElementById('compteur').innerHTML = 'Time spent on Youtube ' + response.timeSpent + ' seconds'
+  })();
 
-
-
-
-//   (async () => {
-//     const response = await chrome.runtime.sendMessage({greeting: "hello"});
-//     // do something with response here, not outside the function
-//     console.log(response);
-//   })();
-
-
-function sendMessagio(){
-
-  chrome.runtime.sendMessage(
-    {
-        message: "messageSent"
-    }, (res) => {console.log(res.status)})
-
-}
-sendMessagio()
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (sendResponse.farewell === "goodbye")
-        {
-            document.getElementById('compteur').innerHTML = 'réussi'
-        }
-    }
-)
