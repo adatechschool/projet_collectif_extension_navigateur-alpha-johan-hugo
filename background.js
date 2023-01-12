79987169b7fbe066fff3d25ece1d85df004a4844
JohanAnquetil <114992907+JohanAnquetil@users.noreply.github.com>
2023-01-12 15:15:41 +0100

    Compteur fonctionnel sur une page déterminée

--- background.js
+++ background.js
@@ -1,26 +1,34 @@
 let seconds = 0;
 
 function counter() {
-    seconds++;
-    document.getElementById('compteur').innerHTML = seconds
+  seconds++
 }
 
 let interval = null;
+let counterActivated = false;
 
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  
+  console.log(counterActivated)
     let [tab] = await chrome.tabs.query(queryOptions);
-    if (tab.url.startsWith('https://www.youtube.com/')) {
-        console.log('Youtube detected');
-        // seconds = 0:
-        interval = setInterval(counter, 1000)
+    if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == false) {
+        interval = setInterval(counter, 1000);
+        console.log("COME ON RED STAR !")
+        counterActivated = true;
+      }
+    else if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == true) {
+      console.log("Retour sur le site Red Star, compteur continue")
     }
-    else {
-      clearInterval(interval);
-      console.log('new tab selected');
-    }
-    console.log(seconds)
+      else {
+        clearInterval(interval);
+        console.log('new tab selected');
+        counterActivated = false;
+      }
+    console.log(seconds);
 }
+
+
+chrome.tabs.onActivated.addListener(getCurrentTab);
 chrome.tabs.onUpdated.addListener(getCurrentTab);
-// chrome.tabs.onActivated.addListener(getCurrentTab);
