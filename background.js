68a2c3868c5952306ad9e0346184ad2182ac3129
abj95630 <abah20000@gmail.com>
2023-01-12 15:32:06 +0100

    Réindentation

--- background.js
+++ background.js
@@ -1,43 +1,39 @@
 let seconds = 0;
+let interval = null;
+let counterActivated = false;
 
 function counter() {
   seconds++
 }
 
-let interval = null;
-let counterActivated = false;
-
 async function getCurrentTab() {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  
+  let queryOptions = { active: true, lastFocusedWindow: true };
   console.log(counterActivated)
-    let [tab] = await chrome.tabs.query(queryOptions);
-    if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == false) {
-        interval = setInterval(counter, 1000);
-        console.log("COME ON RED STAR !")
-        counterActivated = true;
-      }
-    else if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == true) {
-      console.log("Retour sur le site Red Star, compteur continue")
-    }
-      else {
-        clearInterval(interval);
-        console.log('new tab selected');
-        counterActivated = false;
-      }
-    console.log(seconds);
+  let [tab] = await chrome.tabs.query(queryOptions);
+  if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == false) {
+    interval = setInterval(counter, 1000);
+    console.log("COME ON RED STAR !")
+    counterActivated = true;
+  }
+  else if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == true) {
+    console.log("Retour sur le site Red Star, compteur continue")
+  }
+  else {
+    clearInterval(interval);
+    console.log('new tab selected');
+    counterActivated = false;
+  }
+  console.log(seconds);
 }
-
 
 chrome.tabs.onActivated.addListener(getCurrentTab);
 chrome.tabs.onUpdated.addListener(getCurrentTab);
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.greeting === "hello")
-      console.log('bien reçu');
-      sendResponse({timeSpent: seconds}); 
-      return true;
-    }
+  if (request.greeting === "hello")
+    console.log('bien reçu');
+    sendResponse({timeSpent: seconds}); 
+    return true;
+  }
 );
--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 (async () => {
     const response = await chrome.runtime.sendMessage({greeting: "hello"});
-    // do something with response here, not outside the function
     document.getElementById('compteur').innerHTML = 'Time spent on Youtube ' + response.timeSpent + ' seconds'
   })();
 
