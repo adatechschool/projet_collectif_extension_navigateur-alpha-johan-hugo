71d12e08266c7df011107d187223c2614f23a485
JohanAnquetil <114992907+JohanAnquetil@users.noreply.github.com>
2023-01-16 14:54:06 +0100

    A effacer

--- background.js
+++ background.js
@@ -8,29 +8,57 @@ let interval = null;
 let counterActivated = false;
 
 async function getCurrentTab() {
+  
+    detectURL();
     let queryOptions = { active: true, lastFocusedWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  
-  console.log(counterActivated)
     let [tab] = await chrome.tabs.query(queryOptions);
+
     if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == false) {
         interval = setInterval(counter, 1000);
         console.log("COME ON RED STAR !")
         counterActivated = true;
-      }
+    }
     else if (tab.url.startsWith('https://www.redstar.fr/') && counterActivated == true) {
       console.log("Retour sur le site Red Star, compteur continue")
     }
-      else {
-        clearInterval(interval);
-        console.log('new tab selected');
-        counterActivated = false;
-      }
+    else {
+      clearInterval(interval);
+      console.log('new tab selected');
+      counterActivated = false;
+    }
     console.log(seconds);
 }
 
+let tabURL = [];
 
-chrome.tabs.onActivated.addListener(getCurrentTab);
+async function detectURL() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+
+  tabURL.push(tab.url);
+
+  for (i in tabURL) {
+    for (j in tabURL) {
+      if (tabURL[i] == tabURL[j] && i != j) 
+      {
+        console.log("i",tabURL[i],"j",tabURL[j])
+        tabURL.splice(j, 1);
+        console.log('tabURL', tabURL);
+      };
+      // if (tab.URL[i] == tabURL[j]) {
+      // console.log("doublon", tab.URL[j])
+      // }
+    }
+    }
+  }
+
+chrome.tabs.onActivated.addListener((tab) => {
+  getCurrentTab();
+  }
+  );
+
 chrome.tabs.onUpdated.addListener(getCurrentTab);
 
 chrome.runtime.onMessage.addListener(
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 (async () => {
     const response = await chrome.runtime.sendMessage({greeting: "hello"});
     // do something with response here, not outside the function
-    document.getElementById('compteur').innerHTML = 'Time spent on Youtube ' + response.timeSpent + ' seconds'
+    document.getElementById('compteur').innerHTML = 'Time spent on Red Star Fc ' + response.timeSpent + ' seconds'
   })();
 
