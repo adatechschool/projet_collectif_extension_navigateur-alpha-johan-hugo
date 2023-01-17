771b31823b5357762c08470106f9da95a306c3a2
hugorince <hugo.rince@gmail.com>
2023-01-17 11:30:08 +0100

    cleaning

--- background.js
+++ background.js
@@ -7,35 +7,31 @@ const URList = [
   }, 
 ];
 
-
 function counter(n) {
   URList[n].time++  
   console.log('time increased')
 }
 
-async function getCurrentTab() {
+async function timeTracker() {
 
   detectURL();
   let queryOptions = { active: true, lastFocusedWindow: true };
-
   let [tab] = await chrome.tabs.query(queryOptions);
-  
-  var url = tab.url;
-  var urlObject = new URL(url);
-  var domain = urlObject.hostname;
+  let url = tab.url;
+  let urlObject = new URL(url);
+  let domain = urlObject.hostname;
 
   for (let i in URList){
     if (domain == URList[i].url && URList[i].activated == false) {
       URList[i].interval = setInterval(function() {counter(i);}, 1000);
-      console.log("COME ON RED STAR !")
       URList[i].activated = true;
     }
     else if (domain != URList[i].url){
       URList[i].activated = false;
       clearInterval(URList[i].interval);
     }
-  }
-}
+  };
+};
 
 
 async function detectURL() {
@@ -43,48 +39,42 @@ async function detectURL() {
   let [tab] = await chrome.tabs.query(queryOptions);
 
   // Exemple d'URL
-  var url = tab.url;
+  let url = tab.url;
   // Créer un objet URL à partir de l'URL
-  var urlObject = new URL(url);
+  let urlObject = new URL(url);
   // Récupérer le nom de domaine à partir de l'objet URL
-  var domain = urlObject.hostname;
-
+  let domain = urlObject.hostname;
 
   let checkCorrespondance = false
 
   for (i in URList) {
     if (domain == URList[i].url) {
       checkCorrespondance = true;
-    }
+      }
   }
 
-  if (checkCorrespondance == false && domain != "newtab") {
+  if (checkCorrespondance == false && domain != "newtab" && domain != 'extensions'){
     URList.push({
               url: domain,
               activated: false,
               time: 0,
               interval: null }
-              )
+              );
     checkCorrespondance = false;
-            }
-  console.log(URList)
+  };
+  console.log(URList);
+};
 
-}
+chrome.tabs.onActivated.addListener(timeTracker);
+chrome.tabs.onUpdated.addListener(timeTracker);
 
-chrome.tabs.onActivated.addListener(getCurrentTab);
-chrome.tabs.onUpdated.addListener(getCurrentTab);
-
-const comm = (n)=>{
+const comm = () => {
   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
     if (request.greeting === "hello")
       console.log('bien reçu');
-      sendResponse({timeRedStar: URList[0].time}); 
-      sendResponse({timeNantes: URList[1].time}); 
-      URList[n].time = 0
+      //sendResponse({timeRedStar: URList[0].time});
       return true;
     }
   );
-  }
-
-
+};
