c723df2d8d8ab6f15efaa75350f1644da477bad6
JohanAnquetil <114992907+JohanAnquetil@users.noreply.github.com>
2023-01-21 16:58:07 +0100

    temp

--- popup.js
+++ popup.js
@@ -11,18 +11,53 @@ const fillList = (c) => {
     };
 };
 
+let hours = [];
+let minutes = [];
+let seconds = [];
+
+
+const time = (n) => {
+    for (i in n) {
+        console.log("time", n[0])
+       
+        // hours.push(n[i]);  
+    }
+   
+
+}
+
 chrome.storage.local.get().then((result) => {
     let keys = Object.keys(result)
     let values = Object.values(result)
     fillList(keys)
+    time(values)
     button.onclick = () => {
         let sel = document.getElementById('urlist');
         let selectedValue = sel.options[sel.selectedIndex].value;
         console.log(selectedValue)
-        for (let i in values){
-            if (selectedValue == i){
-                document.getElementById('result').textContent = values[i]
+        for (let i in selectedValue){
+            for (i in values){
+            
+                if (n[i] < 60) {
+                    seconds.push(n[i]);
+                    break;
+                    }
+                    else if (n[i] >= 60) {
+                    minutes.push(n[i] / 60);
+                    seconds.push()
+                    hours.push(0)
+                    }
+                    else if (n[i] >= 3600) {
+                    hours.push[n[i] / 3600]
+            
+                    }
+
+            }
+
+            if (selectedValue == i) {
+                document.getElementById('result').textContent = hours[i] + " heures " + minutes[i] + " minutes " + seconds[i] + " secondes "
                 }
+
             };
         };
     }
